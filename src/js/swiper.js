d004337f9334b5b23e7e16c7a7b7ed96497b13be
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-08-20 23:17:50 +0300

    Gigle 1.2.0 SWIPER

---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 50,
+    loop: true,
+    loopedSlides: 3,
+});
+
