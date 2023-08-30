2172a0697c7c0bfc6f7646c79458540ec1d9cecd
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-08-30 16:18:42 +0300

    update

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,21 +1,21 @@
-let timer;
-const closeTimer = document.querySelector('.timer-wrapper');
-document.addEventListener('mousemove', resetTimer);
-document.addEventListener('mousedown', resetTimer);
-document.addEventListener('keypress', resetTimer);
-document.addEventListener('touchmove', resetTimer);
+// let timer;
+// const closeTimer = document.querySelector('.timer-wrapper');
+// document.addEventListener('mousemove', resetTimer);
+// document.addEventListener('mousedown', resetTimer);
+// document.addEventListener('keypress', resetTimer);
+// document.addEventListener('touchmove', resetTimer);
 
-function resetTimer() {
-	clearTimeout(timer);
-	timer = setTimeout(modal, 49000);
-	closeTimer.style = 'display: none;';
-	clearTimeout(closeWindow);
-}
+// function resetTimer() {
+// 	clearTimeout(timer);
+// 	timer = setTimeout(modal, 49000);
+// 	closeTimer.style = 'display: none;';
+// 	clearTimeout(closeWindow);
+// }
 
-function modal() {
-	closeTimer.style = 'display: flex;';
-	closeWindow = setTimeout(() => {
-		closeTimer.style = 'display: none;';
-		window.close();
-	}, 10000);
-}
+// function modal() {
+// 	closeTimer.style = 'display: flex;';
+// 	closeWindow = setTimeout(() => {
+// 		closeTimer.style = 'display: none;';
+// 		window.close();
+// 	}, 10000);
+// }
