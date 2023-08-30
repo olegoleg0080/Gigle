9640058bdc49b98923272859bc6525db6f405242
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-08-30 16:11:28 +0300

    add timer

---
+++ src/js/timer.js
@@ -0,0 +1,21 @@
+let timer;
+const closeTimer = document.querySelector('.timer-wrapper');
+document.addEventListener('mousemove', resetTimer);
+document.addEventListener('mousedown', resetTimer);
+document.addEventListener('keypress', resetTimer);
+document.addEventListener('touchmove', resetTimer);
+
+function resetTimer() {
+	clearTimeout(timer);
+	timer = setTimeout(modal, 49000);
+	closeTimer.style = 'display: none;';
+	clearTimeout(closeWindow);
+}
+
+function modal() {
+	closeTimer.style = 'display: flex;';
+	closeWindow = setTimeout(() => {
+		closeTimer.style = 'display: none;';
+		window.close();
+	}, 10000);
+}
