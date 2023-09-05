4aeecc435e8f6451078346d1eae4c345197dfb63
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-09-05 14:39:15 +0300

    Update contact.js

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,46 +3,45 @@ const inputName = document.querySelector('.input_name');
 const inputContact = document.querySelector('.input_contact');
 
 function shake(element) {
-    element.classList.add('error');
-    element.offsetWidth; // принудительная перерисовка
-    element.classList.remove('error');
+	element.classList.remove('error');
+	element.offsetWidth;
+	element.classList.add('error');
 }
 
 form.addEventListener('submit', event => {
-    event.preventDefault();
-    let valid = true;
+	event.preventDefault();
+	let valid = true;
 
-    if (!inputName.value.trim()) {
-        shake(inputName);
-        valid = false;
-    }
-    if (!inputContact.value.trim()) {
-        shake(inputContact);
-        valid = false;
-    }
+	if (!inputName.value.trim()) {
+		shake(inputName);
+		valid = false;
+	}
+	if (!inputContact.value.trim()) {
+		shake(inputContact);
+		valid = false;
+	}
 
-    const nameRegex = /^[А-ЯҐЄІЇ][а-яґєії']+\s[А-ЯҐЄІЇ][а-яґєії']+$/;
-    if (!nameRegex.test(inputName.value)) {
-        shake(inputName);
-        valid = false;
-    }
+	const nameRegex = /^[А-ЯҐЄІЇ][а-яґєії']+\s[А-ЯҐЄІЇ][а-яґєії']+$/;
+	if (!nameRegex.test(inputName.value)) {
+		shake(inputName);
+		valid = false;
+	}
 
-    if (valid) {
-        localStorage.setItem('name', inputName.value);
-        localStorage.setItem('contact', inputContact.value);
-        localStorage.setItem(
-            'message',
-            document.querySelector('.form__textarea_message').value
-        );
+	if (valid) {
+		localStorage.setItem('name', inputName.value);
+		localStorage.setItem('contact', inputContact.value);
+		localStorage.setItem(
+			'message',
+			document.querySelector('.form__textarea_message').value
+		);
 
-        form.reset();
-    }
+		form.reset();
+	}
 });
 
 inputName.addEventListener('input', () => {
-    inputName.classList.remove('error');
+	inputName.classList.remove('error');
 });
 inputContact.addEventListener('input', () => {
-    inputContact.classList.remove('error');
+	inputContact.classList.remove('error');
 });
-
