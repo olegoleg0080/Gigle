41e349e14ad89eeb4822d4a82afd4832eef9441a
olegoleg0080 <120270714+olegoleg0080@users.noreply.github.com>
2023-09-05 14:33:43 +0300

    ьрьи

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,43 +2,47 @@ const form = document.querySelector('.contact__form');
 const inputName = document.querySelector('.input_name');
 const inputContact = document.querySelector('.input_contact');
 
+function shake(element) {
+    element.classList.add('error');
+    element.offsetWidth; // принудительная перерисовка
+    element.classList.remove('error');
+}
+
 form.addEventListener('submit', event => {
-	event.preventDefault();
-	let valid = true;
+    event.preventDefault();
+    let valid = true;
 
-	if (!inputName.value.trim()) {
-		inputName.classList.remove('error');
-		inputName.classList.add('error');
-		valid = false;
-	}
-	if (!inputContact.value.trim()) {
-		inputContact.classList.remove('error');
-		inputContact.classList.add('error');
-		valid = false;
-	}
+    if (!inputName.value.trim()) {
+        shake(inputName);
+        valid = false;
+    }
+    if (!inputContact.value.trim()) {
+        shake(inputContact);
+        valid = false;
+    }
 
-	const nameRegex = /^[А-ЯҐЄІЇ][а-яґєії']+\s[А-ЯҐЄІЇ][а-яґєії']+$/;
-	if (!nameRegex.test(inputName.value)) {
-		inputName.classList.remove('error');
-		inputName.classList.add('error');
-		valid = false;
-	}
+    const nameRegex = /^[А-ЯҐЄІЇ][а-яґєії']+\s[А-ЯҐЄІЇ][а-яґєії']+$/;
+    if (!nameRegex.test(inputName.value)) {
+        shake(inputName);
+        valid = false;
+    }
 
-	if (valid) {
-		localStorage.setItem('name', inputName.value);
-		localStorage.setItem('contact', inputContact.value);
-		localStorage.setItem(
-			'message',
-			document.querySelector('.form__textarea_message').value
-		);
+    if (valid) {
+        localStorage.setItem('name', inputName.value);
+        localStorage.setItem('contact', inputContact.value);
+        localStorage.setItem(
+            'message',
+            document.querySelector('.form__textarea_message').value
+        );
 
-		form.reset();
-	}
+        form.reset();
+    }
 });
 
 inputName.addEventListener('input', () => {
-	inputName.classList.remove('error');
+    inputName.classList.remove('error');
 });
 inputContact.addEventListener('input', () => {
-	inputContact.classList.remove('error');
+    inputContact.classList.remove('error');
 });
+
